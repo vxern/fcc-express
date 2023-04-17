ac2a7d904c3b832b0cc3727faa2ed180e44ecc1a
vxern <vxern@wordcollector.co.uk>
2023-04-17 09:26:04 +0100

    misc: Complete milestone 2.

--- myApp.js
+++ myApp.js
@@ -1,6 +1,10 @@
 let express = require("express");
 let app = express();
 
+app.get("/", (request, response) => {
+  response.send("Hello Express");
+});
+
 console.log("Hello World");
 
 module.exports = app;
