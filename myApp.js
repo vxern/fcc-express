746c53be08112e8bb864e49fdeacab6ef251a7c4
vxern <vxern@wordcollector.co.uk>
2023-04-17 09:37:36 +0100

    misc: Complete milestone 4.

--- myApp.js
+++ myApp.js
@@ -1,6 +1,8 @@
 let express = require("express");
 let app = express();
 
+app.use("/public", express.static(`${__dirname}/public`));
+
 app.get("/", (request, response) => {
   response.sendFile(`${__dirname}/views/index.html`);
 });
